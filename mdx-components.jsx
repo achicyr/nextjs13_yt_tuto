3418964a222b1bc6ec1b3fee2a60120289c680aa
webdev-archist <webdev.archist@gmail.com>
2023-08-17 20:28:07 +0200

    DONE EPISODE 6 MDX: BLOG EXAMPLE

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -5,7 +5,18 @@ function Ok({children}){
     ahhhhhhhhhh !!!!!!!!!!!
   </li>
 }
+function H1({children}){
+  return <section>
+    hougahouhgag hougaaaa
+    {children}
+    ahhhhhhhhhh !!!!!!!!!!!
+  </section>
+}
 
 export function useMDXComponents(components) {
-  return { li: Ok, ...components }
+  return { 
+    li: Ok, 
+    // h1: H1, 
+    ...components 
+  }
 }
